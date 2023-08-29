8946ffa716e275f6853ae8c1f62b1ebbf0418c71
zhangtory <zhangtory@users.noreply.github.com>
2023-08-29 23:51:55 +0800

    remove alert

--- post.php
+++ post.php
@@ -26,15 +26,9 @@ $this->need('includes/header.php'); ?>
         <div class="mdui-card-media-covered">
           <div class="mdui-card-primary">
             <div class="mdui-card-primary-title"><?php $this->title() ?></div>
-            <div class="mdui-card-primary-subtitle"><?php get_post_view($this) ?> 浏览 | <?php $this->date(); ?> | 阅读时间: <?php readingTime($this->cid); ?> | 分类: <?php $this->category(','); ?> | 标签: <?php $this->tags(','); ?></div>
           </div>
         </div>
       </div>
-      <?php
-        $modified = intval((time() - $this->modified) / 86400);
-        $created = intval((time() - $this->created) / 86400);
-        if($modified > 30){echo '<div class="post-alert mdui-color-red-400 mdui-valign"><i class="mdui-icon material-icons mdui-m-r-2">report</i>请注意，本文编写于 '.$created.' 天前，最后修改于 '.$modified.' 天前，其中某些信息可能已经过时。</div>';}
-      ?>
       <div class="post-content mdui-typo">
         <?php parseContent($this->content); ?>
       </div>
